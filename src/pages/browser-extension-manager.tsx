e4dd51c82811f72ab58e64907976b854feacfbfe
Hades1123 <dp1.1a7.si@gmail.com>
2025-09-04 00:41:49 +0700

    complete light mode browser extension manager

--- src/pages/browser-extension-manager.tsx
+++ src/pages/browser-extension-manager.tsx
@@ -18,8 +18,8 @@ const Button = (props: ButtonProps) => {
     const { children, onClick, className = "", variant } = props;
     const shadowStyle = tw("shadow-[0_1px_2px_0_rgba(184,196,215,0.40)]");
     const variantStyles: Record<Variant, string> = {
-        primary: cn('rounded-[62.4375rem] border-[1px] border-[#D6E2F5] bg-[#FBFDFE]  pt-2 pr-5 pb-[0.625rem] pl-[1.25rem] text-[1.25rem] font-[500]', shadowStyle),
-        remove: tw('rounded-[62.4375rem] border-[1px] border-[#C6C6C6] bg-[#FBFDFE py-2 px-4 text-[1.25rem] font-[500]'),
+        primary: cn('rounded-[62.4375rem] border-[1px] border-[#D6E2F5] bg-[#FBFDFE]  pt-2 pr-5 pb-[0.625rem] pl-[1.25rem] text-[1.25rem] font-[500] focus:outline-2 focus:outline-red-600 focus:outline-offset-4', shadowStyle),
+        remove: tw('rounded-[62.4375rem] border-[1px] border-[#C6C6C6] bg-[#FBFDFE py-2 px-4 text-[1.25rem] font-[500] focus:outline-2 focus:outline-red-500 focus:outline-offset-4'),
     }
     return (
         <button onClick={onClick} className={cn(variantStyles[variant], className)}>
@@ -34,12 +34,12 @@ const BrowserExtension = () => {
 
     return (
         <>
-            <div className='pt-5 px-4 bg-white bg-gradient-to-b from-[#EBF2FC] to-[#EEFBF9] flex gap-10 flex-col font-NotoSans'>
+            <div className='pt-5 px-4 pb-4 bg-white bg-gradient-to-b from-[#EBF2FC] to-[#EEFBF9] flex gap-10 flex-col font-NotoSans min-h-screen'>
                 {/* header */}
                 <div className='flex justify-between rounded-[0.625rem] border-[1px] border-[#D6E2F5] py-[0.5rem] px-[0.75rem]
                 bg-[#FBFDFE] shadow-[0_2px_3px_0_#D9E5F4]'>
                     <img src={logo} alt="logo" />
-                    <div className='w-[3.125rem] h-[3.125rem] rounded-[0.75rem] bg-[#EEE] flex items-center justify-center'>
+                    <div className='w-[3.125rem] h-[3.125rem] rounded-[0.75rem] bg-[#EEE] flex items-center justify-center hover:bg-[#C6C6C6] hover:cursor-pointer focus:ring-2 focus:ring-red-500'>
                         <img src={moon} alt="moon-icon" />
                     </div>
                 </div>
@@ -47,7 +47,7 @@ const BrowserExtension = () => {
                 {/* main title  */}
                 <div className='flex flex-col gap-6 md:flex-row md:justify-between'>
                     <h1 className='text-[2.125rem] font-[700] text-center'>Extensions List</h1>
-                    <div className='flex gap-3'>
+                    <div className='flex gap-3 justify-center'>
                         <Button variant='primary'
                             className={buttonState == 'All' ? 'text-white bg-[#C7231A]' : ''}
                             onClick={() => {
@@ -79,22 +79,54 @@ const BrowserExtension = () => {
                         currentData.map((item, index) => {
                             return (
                                 <div key={index.toString() + item.name}
-                                    className='rounded-[1.25rem] border border-[#D6E2F5] bg-[#FBFDFE] shadow-[0_2px_2px_0_rgba(194,206,225,0.20),0_1px_5px_1px_rgba(194,206,225,0.22)] p-5 flex flex-col gap-6'
+                                    className='rounded-[1.25rem] border border-[#D6E2F5] bg-[#FBFDFE] shadow-[0_2px_2px_0_rgba(194,206,225,0.20),0_1px_5px_1px_rgba(194,206,225,0.22)] p-5 flex flex-col justify-between h-[12.5rem]'
                                 >
                                     {/* main card  */}
                                     <div className='flex gap-4 items-start'>
                                         <img src={item.logo} alt="item-logo" />
                                         <div className='mb-2'>
                                             <h1 className='text-[1.25rem] font-[700]'>{item.name}</h1>
-                                            <p className='text-[#535868]'>{item.description}</p>
+                                            <p className='text-[#535868] line-clamp-4'>{item.description}</p>
                                         </div>
                                     </div>
                                     {/* button  */}
                                     <div className='flex justify-between items-center'>
-                                        <Button variant='remove'>Remove</Button>
-                                        <div className='w-[2.25rem] h-[1.25rem] p-[0.125rem] flex justify-end rounded-[624.9375rem] bg-[#C7231A] hover:cursor-pointer'>
-                                            <span className="size-[1rem] rounded-[624.9375rem] bg-[#FBFDFE] shadow-[0_1px_3px_0_rgba(10,13,18,0.30),0_1px_2px_-1px_rgba(10,13,18,0.30)]"></span>
-                                        </div>
+                                        <Button
+                                            className='hover:bg-[#C7231A] hover:text-white'
+                                            variant='remove'
+                                            onClick={() => {
+                                                setCurrentData(currentData.filter(element => element.name != item.name));
+                                            }}>Remove</Button>
+                                        {
+                                            item.isActive ?
+                                                <button
+                                                    className='w-[2.25rem] h-[1.25rem] p-[0.125rem] flex justify-end rounded-[624.9375rem] bg-[#C7231A] hover:cursor-pointer hover:bg-[#DE4840] focus:outline-2 focus:outline-red-500 focus:outline-offset-4'
+                                                    onClick={() => {
+                                                        setCurrentData(currentData.map(element => {
+                                                            if (element.name == item.name) {
+                                                                element.isActive = false;
+                                                            }
+                                                            return element;
+                                                        }))
+                                                    }}
+                                                >
+                                                    <span className="size-[1rem] rounded-[624.9375rem] bg-[#FBFDFE] shadow-[0_1px_3px_0_rgba(10,13,18,0.30),0_1px_2px_-1px_rgba(10,13,18,0.30)]"></span>
+                                                </button>
+                                                :
+                                                <button
+                                                    className='w-[2.25rem] h-[1.25rem] p-[0.125rem] flex justify-start rounded-[624.9375rem] bg-[#C6C6C6] hover:cursor-pointer focus:outline-2 focus:outline-red-500 focus:outline-offset-4'
+                                                    onClick={() => {
+                                                        setCurrentData(currentData.map(element => {
+                                                            if (element.name == item.name) {
+                                                                element.isActive = true;
+                                                            }
+                                                            return element;
+                                                        }))
+                                                    }}
+                                                >
+                                                    <span className="size-[1rem] rounded-[624.9375rem] bg-[#FBFDFE] shadow-[0_1px_3px_0_rgba(10,13,18,0.30),0_1px_2px_-1px_rgba(10,13,18,0.30)]"></span>
+                                                </button>
+                                        }
                                     </div>
                                 </div>
                             )
